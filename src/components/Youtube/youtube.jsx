1697a8b566af45b04fd431a5338e37bdbe9ab55e
saurabhburewar <58295262+saurabhburewar@users.noreply.github.com>
2020-10-02 00:58:37 +0530

    Add files via upload

---
+++ src/components/Youtube/youtube.jsx
@@ -0,0 +1,55 @@
+/*global chrome*/
+import React, { Component } from "react";
+import Searchbar from "../Searchbar/searchbar";
+import "./youtube.css";
+
+class YouTube extends Component {
+  state = { ytHis: [] };
+
+  componentDidMount() {
+    chrome.history.search(
+      { text: "youtube.com", startTime: 0, maxResults: 10 },
+      (historyItems) => {
+        this.setState({
+          ytHis: [...historyItems],
+        });
+      }
+    );
+  }
+
+  render() {
+    let lT = new Date();
+    let Dateoptions = { year: "numeric", month: "short", day: "numeric" };
+    let Timeoptions = { hour: "numeric", minute: "numeric" };
+    return (
+      <div className="ytPage">
+        <div className="ytHistory">
+          <div className="ytHead">Youtube history</div>
+          {this.state.ytHis.map((ytH) => {
+            lT = new Date(0);
+            lT.setUTCMilliseconds(ytH.lastVisitTime);
+            return (
+              <div key={ytH.id} className="ytHistoryItem">
+                <div className="ytHistoryTitle">
+                  <a className="ythtitle" href={ytH.url}>
+                    {ytH.title}
+                  </a>
+                </div>
+                <div className="ythistoryLastTime">
+                  <div className="ythistoryDate">
+                    {lT.toLocaleDateString("en-US", Dateoptions)}
+                  </div>
+                  <div className="ythistoryTime">
+                    {lT.toLocaleTimeString("en-US", Timeoptions)}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default YouTube;
