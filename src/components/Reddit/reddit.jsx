e65c77b27dfc4a2f9efa378f80f9eee41aa6f99e
saurabhburewar <58295262+saurabhburewar@users.noreply.github.com>
2020-10-02 01:01:38 +0530

    Add files via upload

---
+++ src/components/Reddit/reddit.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Reddit extends Component {
+  state = {};
+  render() {
+    return <div className="redditpage"></div>;
+  }
+}
+
+export default Reddit;
